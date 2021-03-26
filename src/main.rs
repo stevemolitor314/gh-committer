91b747ef17f2211c0d66265bc3f27c1e5aefb2d4
stevemolitor <steve.molitor@zapier.com>
2021-03-26 18:21:16 -0500

    remove some cruft

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,3 @@
-// use reqwest::{
-//     blocking::Client,
-//     header::{HeaderValue, ACCEPT, CONTENT_LENGTH, USER_AGENT},
-// };
-// use std::collections::HashMap;
-
-//fn main() -> Result<(), Box<dyn std::error::Error>> {
-// let url = "https://api.github.com/repos/stevemolitor314/what-time/contents/src/main.rs";
-
-// let client = Client::new();
-// let resp = client
-//     .get(url)
-//     .header(ACCEPT, "application/vnd.github.v3.raw")
-//     .header(CONTENT_LENGTH, HeaderValue::from_static("0"))
-//     .header(USER_AGENT, "gh-committer")
-//     .send()
-//     .text();
-// //.json::<HashMap<String, String>>();
-
-// println!("{:#?}", resp);
-// Ok(())
-//}
-
 use gh_committer::get_file;
 use std::error::Error;
 
